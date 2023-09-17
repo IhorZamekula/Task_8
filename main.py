aa471fa9f95f571318364c7599c91976faf01c5d
IhorZamekula <zamekulaiv@gmail.com>
2023-09-17 21:08:57 +0300

    init

---
+++ main.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+from datetime import date, datetime, timedelta
+
+
+def get_birthdays_per_week(users):
+    # Реалізуйте тут домашнє завдання
+    today_date = date.today()
+    current_year = today_date.year
+    # current_week = today_date + timedelta(days=6)
+    users_list = defaultdict(list)
+   
+    if not users:
+        return {}
+
+    for user in users:
+        name = user['name']    
+        birthday = user['birthday']   
+        birthday_this_year = birthday.replace(year=current_year)
+        if birthday.month == 1:
+            birthday_this_year = birthday.replace(year=current_year + 1)
+        else:
+            birthday_this_year = birthday.replace(year=current_year)
+        if birthday_this_year < today_date:
+            continue
+
+
+        if birthday_this_year.weekday() >= 5:
+            birthday_this_year += timedelta(days=(7 - birthday_this_year.weekday()))
+
+        day_name = birthday_this_year.strftime('%A')
+        users_list[day_name].append(name)    
+
+    return users_list
+ 
+
+if __name__ == "__main__":
+    users = [
+        {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
+    ]
+
+    result = get_birthdays_per_week(users)
+    print(result)
+    # Виводимо результат
+    for day_name, names in result.items():
+        print(f"{day_name}: {', '.join(names)}")
